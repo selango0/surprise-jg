ee6cebb45c039e04b369eec6f83a861ff6b316ff
osman ale <osman.highbrow@gmail.com>
2021-12-25 10:27:13 +0500

    config3

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.set('port', process.env.PORT || 8080);
-
-
 
 
 var server = app.listen(app.get('port'), function() {
