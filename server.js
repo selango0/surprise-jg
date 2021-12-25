366475443be5d445326b7b8611c17d31f88303cc
osman ale <osman.highbrow@gmail.com>
2021-12-25 10:11:05 +0500

    server config2

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
 
+
+
 var server = app.listen(app.get('port'), function() {
 
   console.log('listening on port ', server.address().port);
