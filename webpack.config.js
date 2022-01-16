8cb82b22093a5cb87aaa3cf5ae6c1f6ea95d9841
Shal Sougandini <sys363@jefferson.edu>
2022-01-15 22:07:43 -0500

    chore: use yearn

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
 
 module.exports = {
   output: {
